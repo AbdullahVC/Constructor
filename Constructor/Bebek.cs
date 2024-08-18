6e4f0f3b37f1c746ae5a8bc32a6a69efed7d6129
AbdullahVC <69648705+AbdullahVC@users.noreply.github.com>
2024-08-18 16:18:23 +0300

    Add project files.

---
+++ Constructor/Bebek.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Bebek
+    {
+        private string _name;
+        private string _surname;
+        private DateTime _birthDay;
+
+        // Parametre almayan default constructor
+        public Bebek()
+        {
+            Console.WriteLine("Ingaaaa"); // Konsola Ingaaaa yazısı yazılır
+        }
+
+        // Ad ve Soyad parametrelerini alan constructor
+        public Bebek(string ad, string soyad)
+        {
+            _name = ad;
+            _surname = soyad;
+            _birthDay = DateTime.Now; // Doğum tarihi şu anki zaman olarak atanır
+            Console.WriteLine("Ingaaaa");
+        }
+
+        // Bebek bilgilerini ekrana yazdıran metod
+        public void Info()
+        {
+            Console.WriteLine($"Ad: {_name}, Soyad: {_surname}, Doğum Tarihi: {_birthDay}");
+        }
+
+        // Bebek sınıfının özellikleri
+        public DateTime DogumTarihi
+        {
+            get 
+            { 
+                return _birthDay; 
+            }
+            set
+            {
+                _birthDay = value; 
+            }
+        }
+
+        public string Ad
+        {
+            get 
+            { 
+                return _name; 
+            }
+            private set { 
+                _name = value; 
+            }
+        }
+
+        public string Soyad
+        {
+            get 
+            { 
+                return _surname; 
+            }
+            private set 
+            {
+                _surname = value; 
+            }
+        }
+    }
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Constructor;
+    public class Program
+{
+        public static void Main(string[] args)
+    {
+            // Default constructor
+            Bebek bebek1 = new Bebek();
+
+            // Ad ve Soyad parametrelerini alan constructor
+            Bebek bebek2 = new Bebek("Ali", "Veli");
+            bebek2.Info();
+    }
+}
